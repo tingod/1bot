86d5754589872d2c0319245380b046960464fd4b
pugu <tingodl@gmail.com>
2018-07-08 22:17:23 +0800

    Bug fix

--- models/tuling.py
+++ models/tuling.py
@@ -4,11 +4,11 @@ from utils.utils import sj, s
 
 def tuling(message, session):
     tm = Tuling()
-    data = {
+    params = {
         'info': message.content,
         'userid': message.source
     }
-    rtn = tm.fetch(**data)
+    rtn = tm.fetch(params)
     if 'url' in rtn:
         """
         图灵用的是360搜索，在此取其json格式解析
@@ -31,10 +31,10 @@ class Tuling(object):
     __URL = 'http://www.tuling123.com/openapi/api'
     __KEY = "f8d28b3c9d6048759f3bebc164dd8f02"
 
-    def fetch(self, **data):
-        data['key'] = self.__KEY
-        return sj(self.__URL, **data)
+    def fetch(self, params):
+        params['key'] = self.__KEY
+        return sj(self.__URL, params)
 
-    def text(self, **data):
-        r = self.fetch(**data)
+    def text(self, params):
+        r = self.fetch(params)
         return r['text']
